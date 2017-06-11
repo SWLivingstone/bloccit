c00af2f4c1d102136bbf5ae7aa303374ba622bd4
SWLivingstone <scottwlivingstone@gmail.com>
2017-06-11 12:47:00 -0700

    Checkpoint 19 assignment.  Add model, controller, and views to allow for the creating of adds.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :posts
+  resources :posts, :advertisements
+
+  get 'advertisements/show'
 
   get 'about' => 'welcome#about'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170609225840) do
+ActiveRecord::Schema.define(version: 20170611190718) do
+
+  create_table "advertisements", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
