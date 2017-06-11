337f6ebb4745301670649a24ddd0b2ce6d9a1845
SWLivingstone <scottwlivingstone@gmail.com>
2017-06-10 21:45:26 -0700

    Checkpoint 18 assignment.  Add parameter to PostsController to change the first and every 5th post to "SPAM".

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,16 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.first.update(
+      title: "SPAM"
+    )
+    @posts.each do |post|
+      if (post.id - 1) % 5 == 0
+        post.update(
+          title: "SPAM"
+        )
+      end
+    end
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,3 @@
 class Post < ActiveRecord::Base
   has_many :comments
-
-  self.all.each do |post|
-    if (post.id - 1) % 5 == 0
-      post.update!(
-        title: "SPAM"
-      )
-    end
-    self.first.update(
-      title: "SPAM"
-    )
-  end
 end
