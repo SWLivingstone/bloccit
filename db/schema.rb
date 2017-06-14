e6e1b559a5d49143ee4f9263942dea79c147c383
SWLivingstone <scottwlivingstone@gmail.com>
2017-06-13 20:59:57 -0700

    Checkpoint-22.  Add topics.  Nest posts under topics.

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170609225840) do
-
-  create_table "answers", force: :cascade do |t|
-    t.text     "body"
-    t.integer  "question_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
+ActiveRecord::Schema.define(version: 20170614020245) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -34,7 +27,10 @@ ActiveRecord::Schema.define(version: 20170609225840) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "topic_id"
   end
+
+  add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
@@ -42,6 +38,14 @@ ActiveRecord::Schema.define(version: 20170609225840) do
     t.boolean  "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "public",      default: true
+    t.text     "description"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
