3bd89baa957918f346cfcb299e7e09c2f2bb3d62
SWLivingstone <scottwlivingstone@gmail.com>
2017-06-16 19:08:40 -0700

    Checkpoint 27.  Assign User to Posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+# Create Users
+5.times do
+  User.create!(
+    name: "#{RandomData.random_word} #{RandomData.random_word}",
+    email: "#{RandomData.random_word}@#{RandomData.random_word}.com",
+    password: "password"
+  )
+end
+
+
 # Create Topics
 15.times do
   Topic.create!(
@@ -6,13 +16,15 @@
   )
 end
 topics = Topic.all
+users = User.all
 
 # Create Posts
 50.times do
   Post.create!(
     topic:  topics.sample,
     title:  RandomData.random_sentence,
-    body:   RandomData.random_paragraph
+    body:   RandomData.random_paragraph,
+    user: users.sample
   )
 end
 posts = Post.all
@@ -25,7 +37,15 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  name: 'Scott',
+  email: 'scottwlivingstone@gmail.com',
+  password: 'password'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
