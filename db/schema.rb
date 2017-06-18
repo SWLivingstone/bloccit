0c81d640423b423439a165601efc7ac5b788fbb8
SWLivingstone <scottwlivingstone@gmail.com>
2017-06-17 17:20:06 -0700

    Checkpoint 28.  Add athorization tiers for Admin, Member, and Guest.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170617002937) do
+ActiveRecord::Schema.define(version: 20170617222900) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20170617002937) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -37,11 +37,19 @@ posts = Post.all
   )
 end
 
-user = User.first
-user.update_attributes!(
-  name: 'Scott',
-  email: 'scottwlivingstone@gmail.com',
-  password: 'password'
+# Create a admin user
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+# Create a member
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
 )
 
 puts "Seed finished"
