a775b3c5fc23d1e76afa072ca03fdf958da0a47b
Scott Livingstone <scottwlivingstone@gmail.com>
2017-06-22 21:20:56 -0700

    Checkpoint 32 assignment.  Refactor model and vote spec to use factorygirl.  Updated user show view to show favorited posts.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  def has_created_post?
+    current_user.posts.first
+  end
+
+  def has_made_comment?
+    current_user.comments.first
+  end
+
+  def has_favorite_posts?
+    current_user.favorites.first
+  end
 end
